da9f048d3aaa17f5ac13ddb831d4f47502d52a9e
Abinash11-06-98 <abinash.singha.fit.cse17@teamfuture.in>
2022-05-05 12:46:43 +0530

    added : Days of week

---
+++ weekday.sh
@@ -0,0 +1,20 @@
+read -p "enter the num" num
+$day 
+
+  if (( num==1))
+ then  echo "sunday"
+elif ((num == 2))
+ then echo "monday"
+elif ((num == 3))
+  then echo "tuesday"
+elif ((num == 4)) 
+then echo "wednesday"
+elif (( num == 5 ))
+ then echo "thusday"
+elif ((num == 6))
+then echo "friday"
+ elif  ((num == 7))
+then echo "saturday"
+else 
+    echo 
+fi

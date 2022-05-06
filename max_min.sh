bef3e4670bcd16192024e27e5c9522621f1e0c5f
Abinash11-06-98 <abinash.singha.fit.cse17@teamfuture.in>
2022-05-06 20:18:15 +0530

    added : use_ifelse_max_minvalue

---
+++ max_min.sh
@@ -0,0 +1,12 @@
+echo  num1==123 
+echo num2==133 
+echo num3==144 
+echo num4==155
+echo num5==166
+read -p "enter the num" num
+ if (( num==166 ))
+    
+ then echo " max"
+else ((num==123))
+  echo "min"
+fi

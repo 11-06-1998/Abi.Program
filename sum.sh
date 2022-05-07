5f683b7e8719a8b9842608a094fd420540e100b5
Abinash11-06-98 <abinash.singha.fit.cse17@teamfuture.in>
2022-05-08 02:12:19 +0530

    added: calculation_sum

---
+++ sum.sh
@@ -0,0 +1,14 @@
+ read -p  "enter numberA" numA 
+ read -p "enter numberB"  numB
+ read -p "enter numberC"  numC
+ 
+# sum logic
+ sum=$(( (numA+numB)/numC ))
+     echo "sum is" $sum
+# sum1 logic
+
+  sum1=$(( (numC+numA)/numB ))
+   echo "sum is" $sum1
+ 
+  
+
